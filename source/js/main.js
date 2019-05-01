0134fc4ed3bf2f0c9f9ccd0557152c807ff7a599
Ryo Chiba <ryochiba@gmail.com>
2019-05-01 14:29:56 -0700

    Add Google Search Console landing page

--- source/js/main.js
+++ source/js/main.js
@@ -24,20 +24,6 @@
 window.addEventListener('DOMContentLoaded', function (){
     'use strict';
     window['unicornplatform'] = {};
-    // START OF: console.log greeting =====
-    var greetCoders = (function(){
-    	function madeWith(){
-            //Console Eacter Egg
-            console.log('This website was made with ⤵');
-            console.log('%c Unicorn Platform \n', 'font-weight: bold; font-size: 30px;color: red; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113); padding-bottom: 15px;padding-top: 15px;');
-    		console.log('https://unicornplatform.com');
-    	}
-    	return {
-    		madeWith: madeWith
-    	}
-    }());
-
-    greetCoders.madeWith();
     // ===== END OF: console.log greeting
 
 
